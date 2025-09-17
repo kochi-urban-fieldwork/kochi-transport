69e4a67845be6dd15924282534f808f96b09809a
Chandra-Madhubani <chandra20052020@gmail.com>
2025-09-17 11:45:41 +0530

    Update qgis2web.js

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -480,13 +480,13 @@ var Abstract = new ol.control.Control({
             window.showAbstract = function() {
                 linkElement.classList.remove("project-abstract");
                 linkElement.classList.add("project-abstract-uncollapsed");
-                linkElement.innerHTML = '<h4>With the transportation theme, we aim to understand the<br> accessibility of the various modes of transport within Kochi, encapsulating<br>all three dimensions of sustainability<br>(environmental, economic, and social).</h4><p style="font-size: 30px;"> Click on a ward to see information for it. <br />Click the top-right button to hide/reveal wards or points. <br />Navigate to the rest of themes with the links in the bottom left.</p>';
+                linkElement.innerHTML = '<h4>With the transportation theme, we aim to understand the<br> accessibility of the various modes of transport within Kochi, encapsulating<br>all three dimensions of sustainability<br>(environmental, economic, and social).</h4><p style="font-size: 50px;"> Click on a ward to see information for it. <br />Click the top-right button to hide/reveal wards or points. <br />Navigate to the rest of themes with the links in the bottom left.</p>';
             }
 
             hideAbstract();
         } else {
             linkElement.classList.add("project-abstract-uncollapsed");
-            linkElement.innerHTML = '<h4>With the transportation theme, we aim to understand <br>the accessibility of the various modes of transport within Kochi, encapsulating all three<br>dimensions of sustainability<br>(environmental, economic, and social).</h4><p style="font-size: 30px;">Click on a ward to see information for it. <br />Click the top-right button to hide/reveal wards or points. <br />Navigate to the rest of themes with the links in the bottom left.</p>';
+            linkElement.innerHTML = '<h4>With the transportation theme, we aim to understand <br>the accessibility of the various modes of transport within Kochi, encapsulating all three<br>dimensions of sustainability<br>(environmental, economic, and social).</h4><p style="font-size: 50px;">Click on a ward to see information for it. <br />Click the top-right button to hide/reveal wards or points. <br />Navigate to the rest of themes with the links in the bottom left.</p>';
         }
 
         titleElement.appendChild(linkElement);
@@ -975,5 +975,6 @@ document.addEventListener('DOMContentLoaded', function() {
         bottomRightContainerDiv.appendChild(attributionControl);
 
     }
+
 
 
